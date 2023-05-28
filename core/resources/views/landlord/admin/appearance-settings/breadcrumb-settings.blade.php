bbfe567d89d20147b3f9859e28fb49be6a2dcf16
Xgenious <xgenious@Xgeniouss-Mac-mini-2.local>
2023-05-28 16:19:16 +0600

    NumberToWordHelper added

--- core/resources/views/landlord/admin/appearance-settings/breadcrumb-settings.blade.php
+++ core/resources/views/landlord/admin/appearance-settings/breadcrumb-settings.blade.php
@@ -14,17 +14,30 @@
                     @csrf
 
                     @tenant
-                        <x-fields.media-upload name="background_image_one" title="{{__('Image One')}}"/>
-                        <x-fields.media-upload name="background_image_two" title="{{__('Image Two')}}"/>
-                        <x-fields.media-upload name="background_image_three" title="{{__('Image Three')}}"/>
-                        <x-fields.media-upload name="background_image_four" title="{{__('Image Four')}}"/>
-                        <x-fields.media-upload name="background_image_five" title="{{__('Image Five')}}"/>
+                        <p class="alert alert-info">{{__('The Breadcrumb will be applicable if the theme contains breadcrumb images')}}</p>
+                        @php
+                            $hasValue = false;
+                            switch(getSelectedThemeSlug()){
+                                case 'aromatic':
+                                $image_count = 5;
+                                $hasValue = true;
+                                break;
+                            }
+                        @endphp
+
+                        @if($hasValue)
+                            @for($i=1; $i<=$image_count; $i++)
+                                <x-fields.media-upload name="background_image_{{number_to_word($i)}}" title="{{__('Image '.ucfirst(number_to_word($i)))}}"/>
+                            @endfor
+
+                            <button type="submit" class="btn btn-gradient-primary mt-5 me-2">{{__('Save Changes')}}</button>
+                        @endif
                     @else
                         <x-fields.media-upload name="background_left_shape_image" title="{{__('Left Shape Image')}}"/>
                         <x-fields.media-upload name="background_right_shape_image" title="{{__('Right Shape Image')}}"/>
-                    @endtenant
 
-                    <button type="submit" class="btn btn-gradient-primary mt-5 me-2">{{__('Save Changes')}}</button>
+                        <button type="submit" class="btn btn-gradient-primary mt-5 me-2">{{__('Save Changes')}}</button>
+                    @endtenant
                 </form>
             </div>
         </div>
