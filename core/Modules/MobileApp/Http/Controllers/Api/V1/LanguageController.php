f656648f04820ed05567ca311fbd2fdebcbad6a3
iamsuzon <misujon01@gmail.com>
2023-08-26 13:40:10 +0600

    mobile api language bug fix

--- core/Modules/MobileApp/Http/Controllers/Api/V1/LanguageController.php
+++ core/Modules/MobileApp/Http/Controllers/Api/V1/LanguageController.php
@@ -3,33 +3,33 @@
 namespace Modules\MobileApp\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Language;
+use App\Models\Language;
 use Illuminate\Http\Request;
 
 
 class LanguageController extends Controller
 {
     public function languageInfo(){
-        
+
         $languages = Language::select('id','name','slug','direction')->where('default',1)->first()->toArray();
-        
+
         if(!is_null($languages)){
             return response()->json([
                 'language'=>$languages,
             ]);
         }
-        
+
         return response()->json([
-                'language'=> [ 
+                'language'=> [
                     "slug" => "en_GB",
                     "direction" => "ltr"
                 ],
         ]);
     }
-    
+
     public function translateString(Request $request){
         $translateable_array = json_decode($request->get('strings'),true);
-        
+
         $translated_array = [];
         if($request->has('strings')){
             foreach($translateable_array as $key => $string){
@@ -41,6 +41,4 @@ class LanguageController extends Controller
             'strings'=> $translated_array
         ]);
     }
-
-    
 }
