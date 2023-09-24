33f2c00916229997c475582e9f2574562278a4de
iamsuzon <misujon01@gmail.com>
2023-09-24 18:44:56 +0600

    tenants table data column repair

--- core/app/Console/Commands/TenantCleanup.php
+++ core/app/Console/Commands/TenantCleanup.php
@@ -7,6 +7,7 @@ use App\Models\Tenant;
 use AWS\CRT\Log;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class TenantCleanup extends Command
 {
@@ -31,16 +32,27 @@ class TenantCleanup extends Command
      */
     public function handle()
     {
-
         Config::set("database.connections.mysql.engine","InnoDB");
         Tenant::where("cleanup",0)->latest()->chunk(50,function ($tenans){
             foreach ($tenans as $tenant){
                 \DB::table("tenants")->where("id",$tenant->id)->update(["cleanup" => 1]);
                 TenandOperations::dispatch($tenant);
+                $this->repareDataColumn($tenant);
             }
         });
 
         \Illuminate\Support\Facades\Log::alert('tenant cleanup started');
         return Command::SUCCESS;
+    }
+
+    private function repareDataColumn($tenant)
+    {
+        $selected_tenant = DB::table('tenants')->where('id', $tenant->id)->first();
+        $data = $selected_tenant->data;
+        $new_json = json_decode($data);
+        unset($new_json->cleanup, $new_json->is_renew, $new_json->start_date, $new_json->expire_date, $new_json->unique_key, $new_json->renew_status);
+        $new_encode = json_encode($new_json);
+
+        DB::table('tenants')->where('id', $tenant->id)->update(['data' => $new_encode]);
     }
 }
