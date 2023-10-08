161b39c938a3f379c4c37ec79ccef5addde8e09f
iamsuzon <misujon01@gmail.com>
2023-10-08 20:06:55 +0600

    trust host security issue solved

--- core/app/Http/Middleware/TrustHosts.php
+++ core/app/Http/Middleware/TrustHosts.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\CustomDomain;
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
 class TrustHosts extends Middleware
@@ -15,6 +16,13 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
+            $this->customDomains()
         ];
+    }
+
+    private function customDomains()
+    {
+        $customDomains = CustomDomain::where('custom_domain_status', 'connected')->pluck('custom_domain')->toArray();
+        return count($customDomains) > 2 ? implode(',', $customDomains) : current($customDomains);
     }
 }
